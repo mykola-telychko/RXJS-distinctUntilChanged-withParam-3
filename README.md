15387bcac66408482fa7a9bb0db08538d11c518e
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-20 23:42:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-distinctUntilChanged-withParam-3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ftefki)
---
+++ index.ts
@@ -0,0 +1,16 @@
+import { from } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
+
+// only output distinct values, based on the last emitted value
+const source$ = from([
+  { name: 'Brian' },
+  { name: 'Joe' },
+  { name: 'Joe' },
+  { name: 'Sue' },
+]);
+
+source$
+  // custom compare based on name
+  .pipe(distinctUntilChanged((prev, curr) => prev.name === curr.name))
+  .subscribe(console.log);
+// output: { name: 'Brian }, { name: 'Joe' }, { name: 'Sue' }
